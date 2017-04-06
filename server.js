2ae7233bb798f83fe307ee9e6287ec997e42fcc8
Javier Negre <javi.negre@gmail.com>
2017-04-06 20:22:45 +0200

    Add slides route, remove unused 'interanual'

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express'),
   app = express();
 
-const HomeApp = require('./apps/home/index');
-  // InteranualApp = require('./apps/interanual/index');
-  // SlidesApp = require('./apps/slides/index');
+const HomeApp = require('./apps/home/index'),
+  SlidesApp = require('./apps/slides/index');
 
-// app.use('/interanual/', InteranualApp);
-// app.use('/slides/', SlidesApp);
+app.use('/slides/', SlidesApp);
 app.use('/', HomeApp);
 
 app.listen(8080, () => { console.log('Listening to 8080'); });
