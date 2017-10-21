2ed5bfd371f83eccd1eb3ea6b867c8e61afd4a1b
Javier Negre <javi.negre@gmail.com>
2017-10-21 22:56:58 +0200

    Add bicing route

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express'),
-  app = express();
+const express = require('express');
+const app = express();
 
-const HomeApp = require('./apps/home/index'),
-  SlidesApp = require('./apps/slides/index');
+const BicingApp = require('./apps/bicing/index');
+const HomeApp = require('./apps/home/index');
+const SlidesApp = require('./apps/slides/index');
 
+app.use('/bicing/', BicingApp);
 app.use('/slides/', SlidesApp);
 app.use('/', HomeApp);
 
