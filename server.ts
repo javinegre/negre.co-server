a5e6ce6bd7be40e92e392fbec16fa9dbbe099ce0
Javier Negre <javi.negre@gmail.com>
2022-11-29 16:17:34 +0100

    New bicing API route

--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import { Express } from 'express';
 const express = require('express');
 const app: Express = express();
 
+const BicingApi = require('./apis/bicing-api/index');
+
 const BicingApp = require('./apps/bicing-2021/index');
 const BicingVueApp = require('./apps/bicing-vue/index');
 const HomeApp = require('./apps/home/index');
@@ -11,6 +13,7 @@ const SlidesApp = require('./apps/slides/index');
 app.use('/.well-known', express.static(__dirname + '/well-known-folder'));
 app.use('/files', express.static(__dirname + '/public-files'));
 
+app.use('/bicing/api/', BicingApi);
 app.use('/bicing/', BicingApp);
 app.use('/bicing-vue/', BicingVueApp);
 app.use('/slides/', SlidesApp);
